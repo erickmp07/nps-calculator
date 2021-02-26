113573da0a99e75338a83a7927e4f62ed5b8db55
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-02-26 11:42:09 -0300

    Refactor code style

--- src/__tests__/User.test.ts
+++ src/__tests__/User.test.ts
@@ -11,23 +11,23 @@ describe("Users", () => {
 
     it("Should be able to create a new user", async () => {
         const response = await request(app)
-        .post("/users")
-        .send({
-            email: "user@example.com", 
-            name: "User Example"
-        });
+            .post("/users")
+            .send({
+                email: "user@example.com", 
+                name: "User Example"
+            });
 
         expect(response.status).toBe(201);
     });
 
     it("Should not be able to create a user with existing email", async () => {
         const response = await request(app)
-        .post("/users")
-        .send({
-            email: "user@example.com", 
-            name: "User Example"
-        });
+            .post("/users")
+            .send({
+                email: "user@example.com", 
+                name: "User Example"
+            });
 
         expect(response.status).toBe(400);
-    })
+    });
 });
