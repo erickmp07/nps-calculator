dee72a604bb9eaa9d3b7cacffac40cee810b805f
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-02-26 01:26:39 -0300

    Refactor User Controller to use User Repository

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,12 +1,12 @@
 import { Request, Response } from "express";
-import { getRepository } from "typeorm";
-import { User } from "../models/User";
+import { getCustomRepository } from "typeorm";
+import { UserRepository } from "../repositories/UserRepository";
 
 class UserController {
     async create(request: Request, response: Response) {
         const { name, email } = request.body;
 
-        const userRepository = getRepository(User);
+        const userRepository = getCustomRepository(UserRepository);
 
         const userAlreadyExists = await userRepository.findOne({
             email
@@ -28,4 +28,4 @@ class UserController {
     }
 }
 
-export { UserController }
+export { UserController };
